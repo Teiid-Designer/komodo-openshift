29304b2b69e85e84a0df053725665e523d1b1d5e
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2017-04-27 21:41:41 +0100

    Adds komodo and vdb-bench as submodules
    * changes the submodules to the dv-6.4.x branches

--- source/bin/init-submodules.sh
+++ source/bin/init-submodules.sh
@@ -1,2 +1,25 @@
+#!/bin/bash
+
+BRANCH="dv-6.4.x"
+MODULES=(komodo vdb-bench)
+
 git submodule update --init
-git submodule foreach git submodule update --init
+git submodule foreach \
+  git submodule update --init;
+
+ROOT=${PWD}
+for var in "${MODULES[@]}"
+do
+  echo "${var}"
+  cd ${ROOT}/${var}
+  
+  if [ `git branch --list ${BRANCH}` ]; then
+    git checkout ${BRANCH}
+  else
+    git checkout -b ${BRANCH} origin/${BRANCH}
+  fi
+
+  cd ${ROOT}
+done
+
+
